87ae0e786b5a14d369f9d58786417593b7861841
shakhbozv <shakhbozv@gmail.com>
2023-04-15 15:54:12 +0500

    fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import Layout from '../../components/Layout/Layout'
 import Rooms from '../../components/Rooms/Rooms'
 import "./home.css"
 import { BrowserRouter , Routes, Route } from 'react-router-dom'
-import Layout from "../../components/Layout/Layout";
 import Header from "../../components/Header/Header";
 import CheckIn from "../../components/CheckIn/CheckIn";
 import About from "../../components/About/About";
