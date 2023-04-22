914e24ee12ba7962aa1a0a768ad612e563be9fd3
unknown <Ozodbek@gmail.com>
2023-04-22 16:06:34 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter , Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home/Home";
 import Pool from "./pages/Pool/Pool";
+import RoomDetail from './pages/RoomDetail/RoomDetail'
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/pool' element={<Pool/>}/>
+                <Route path='/room-detail/:id' element={<RoomDetail/>}/>
             </Routes>
         </BrowserRouter>
     )
