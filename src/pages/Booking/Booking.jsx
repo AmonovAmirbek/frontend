99a253d9bbcd587f9bc138b7e54c048d7456c112
Shaxboz1 <shaxbozoripov013@gmail.com>
2023-05-06 16:31:33 +0500

    added about and booking

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -10,20 +10,21 @@ function Booking() {
 
         <div className='booking__section-images__columns'>
           <div className='images__column left'>
-            <img src="./img/about-1.jpg" alt="" width="250px" height="190px"></img>
+            <img src="./img/about-1.jpg" alt="" width="250px" height="210px"></img>
             <img src="./img/about-2.jpg" alt="" width="270px" height="240px"></img>
           </div>
           <div className='images__column right'>
-            <img src="./img/about-3.jpg" alt="" width="190px" height="160px"></img>
-            <img src="./img/about-4.jpg" alt="" width="260px" height="180px"></img>
+            <img src="./img/about-3.jpg" alt="" width="220px" height="200px"></img>
+            <img src="./img/about-4.jpg" alt="" width="250px" height="220px"></img>
           </div>
 
         </div>
-        <form className='form-css'>
-          <input type="text" name="field2" class="field-long" placeholder='Your Name' />
-          <input type="email" name="field3" class="field-long" placeholder='Your Email' />
-          <input type="email" name="field3" class="field-long" placeholder='Check In' />
-          <input type="email" name="field3" class="field-long" placeholder='Check Out' />
+        <form className='form-css'>    
+          <input type="text" name="field2" className="field-long" placeholder='Your Name' />
+          <input type="email" name="field3" className="field-long" placeholder='Your Email' />
+          <input type="text" name="field3" className="fullwidth" placeholder='Your Phone' />
+          <input type="date" name="field3" className="field-long" placeholder='Check In' onChange={e => console.log(e.target.value)}/>
+          <input type="date" name="field3" className="field-long" placeholder='Check Out' />
 
           <select name="field4" className="halfwidth" placeholder='Adult 1'>
             <option value="Advertise" placeholder='Select Adult'>Adult 1</option>
@@ -36,16 +37,7 @@ function Booking() {
             <option value="Partnership">Child 2</option>
             <option value="General Question">Child 3</option>
           </select>
-
-          <select name="field4" className="fullwidth" placeholder='Room 1'>
-            <option value="Advertise">Room 1</option>
-            <option value="Partnership">Room 2</option>
-            <option value="General Question">Room 3</option>
-          </select>
-
-          <textarea name="field5" id="field5" class="fullwidth" placeholder='Special Request'></textarea>
-
-          <button className='booking_button'>Book Now</button>
+          <button className='booking_button'>Book Now</button> 
 
         </form>
       </div>
